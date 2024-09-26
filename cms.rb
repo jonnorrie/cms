3a244f988f17957ab59f61ca2ab1754255241a60
EC2 Default User <ec2-user@ip-172-31-20-25.us-east-2.compute.internal>
2024-09-26 16:35:32 +0000

    add markdown rendering to .md files

--- cms.rb
+++ cms.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "redcarpet" #Used to render Markdown text into HTML
 
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
+end
+
+helpers do
+  
+  def render_markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    # This is a new instance of Redcarpet
+    
+    markdown.render(text)
+  end
+  
 end
 
 root = File.expand_path("..", __FILE__)
@@ -36,22 +48,29 @@ get "/:filename" do
   end
   
   if @files.include?(params[:filename])
-
     file_path = root + "/data/" + params[:filename]
-    # `file_path` is just a string of the file path.
-    # params is a hash given to us through sinatra.
-    # when we type a URL with `:filename` being the name of a file,
-    # `:filename` becomes the key, and the file in the URL is the value.
-    # This allows us to dynamically handle the route without creating
-    # a route for every file
+     # `file_path` is just a string of the file path.
+      # params is a hash given to us through sinatra.
+      # when we type a URL with `:filename` being the name of a file,
+      # `:filename` becomes the key, and the file in the URL is the value.
+      # This allows us to dynamically handle the route without creating
+      # a route for every file
+  
+    if File.extname(file_path) == ".md"
+      #  if the file type is `.md` we render it in markdown
     
-    headers["Content-Type"] = "text/plain"
-    # This sets the content type header of the request 
-    # to just plain text.
+      file_text = File.read(file_path)
     
-    File.read(file_path)
-    # if the file requested exists, it is read using
-    # the `File.read` method with the file path passed in.
+      render_markdown(file_text)
+    else
+
+      headers["Content-Type"] = "text/plain"
+      # This sets the content type header of the request 
+      # to just plain text.
+      
+      File.read(file_path)
+      # reads the `File.read` method with the file path passed in.
+    end
   else
     session[:message] = "#{params[:filename]} does not exist"
     
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -54,5 +54,28 @@ include Rack::Test::Methods
     assert_includes last_response.body, "Ruby 0.95 released"
   end
   
+  def test_document_not_found
+    get "/notafile.ext" # Attempt to access a nonexistent file
+  
+    assert_equal 302, last_response.status # Assert that the user was redirected
+  
+    get last_response["Location"] # Request the page that the user was redirected to
+  
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+  
+    get "/" # Reload the page
+    refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed
+  end
+  
+  # test/cms_test.rb
+  def test_viewing_markdown_document
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
+  end
+  
 
 end
